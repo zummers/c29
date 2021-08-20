69eb72c17cb8c20b65e873e2cf075991c9316784
zummers <83545395+zummers@users.noreply.github.com>
2021-08-20 19:06:25 +0100

    Add files via upload

---
+++ C29_SA1-main/link.js
@@ -0,0 +1,14 @@
+class Link{
+    constructor(bodyA,bodyB){
+        var lastlink=bodyA.body.bodies.length-2
+        this.link=Constraint.create({
+            bodyA:bodyA.body.bodies[lastlink],
+            pointA:{x:0,y:0},
+            bodyB:bodyB,
+            pointB:{x:0,y:0},
+            length:-5,
+            stiffness:0.1
+        })
+        World.add(engine.world,this.link)
+    }
+}
